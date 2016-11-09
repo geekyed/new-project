7b21dcbc7cdc5bdfe7f02276084dde8351a9f32b
Edward Weston <eddweston@me.com>
2016-11-09 11:46:25 +0000

    basics

--- src/app.js
+++ src/app.js
@@ -1,15 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { TodoList } from './components/TodoList.jsx'
-
-const dummyTodos = [
-  { id: 0, isDone: true, text: 'make components' },
-  { id: 1, isDone: false, text: 'design actions' },
-  { id: 2, isDone: false, text: 'implement reducer' },
-  { id: 3, isDone: false, text: 'connect components' }
-]
 
 render(
-  <TodoList todos={dummyTodos} />,
+  <h1>'Hello World'</h1>,
   document.getElementById('app')
 )
